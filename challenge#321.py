0043cf2c62e69d04c6fc171b19f9dd189d8952f5
Clockwired <35982951+Clockwired@users.noreply.github.com>
2018-01-31 18:25:36 -0500

    challenge#321.py
    Just some modifications.

--- challenge#321.py
+++ challenge#321.py
@@ -1,11 +1,15 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/6jr76h/20170627_challenge_321_easy_talking_clock/
 # This is the challenge if ever to refer back to it.
 
+#Check
+p = 10
+l = 5
+
 '''Checks and outputs 24 hour time'''
 def twelve_hour_time_fix(x, n):
 	if type(x) == int and type(n) == int:
 		if x == 0:
-			return("Error")
+			return("BAWK BAWK STOP IT")
 		elif n == 0 and x <= 12:
 			return ("it's " + x + " oh clock")
 		elif n > 0 and n < 10 and x <= 12:
@@ -16,8 +20,8 @@ def twelve_hour_time_fix(x, n):
 			return("error, please use 12 hour time")
 	else: 
 		return("oh noes, there's an error, actually use non float and non string inputs!")
-    
-# The function above is just a combination of string formatting and control flow, nothing hard
+
+# These functions I made reuqire string formatting
 
 ''' This is the better version of my above function '''
 def twenty_four_hour_time_fix(x, n):
@@ -30,6 +34,10 @@ def twenty_four_hour_time_fix(x, n):
 			meridian = 'p.m'
 			if x > 12:
 				return("Error")
+			elif x == 12 and n < 10 and n > 0:
+				return("It's " + str(x) + " oh 0" + str(n) + " " + "p.m")
+			elif x == 12 and n < 60 and n >= 10:
+				return("It's " + str(x) + " oh " + str(n) + " " + "p.m")
 			elif x <= 12 and n < 10 and n > 0:
 				return("It's " + str(x) + " oh 0" + str(n) + " " + str(meridian))
 			elif x <= 12 and n >= 10 and n < 60:
@@ -48,5 +56,8 @@ def twenty_four_hour_time_fix(x, n):
 			elif n > 60:
 				return("Error")
 			else:
-				return("Oh Noes, It's an error!")
-        
+				return("Oh Noes")
+
+print(twenty_four_hour_time_fix(p, l))
+
+print(twelve_hour_time_fix(p, l))
